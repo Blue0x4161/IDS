51325ee544a053dd664a7e6d77426228152ca704
offline707 <ayataabueid@gmail.com>
2025-11-22 21:10:08 +0300

    Added the threads part

--- ioc_manager.py
+++ ioc_manager.py
@@ -9,11 +9,9 @@ class ioc_manager:
         self.malicious_ips = {"ips": ioc_fetcher.ioc_fetcher.combine_ioc_sets()}
 
     def auto_update(self, interval_hours=2):
-        while True:
-            print("[+] Updating IOCs ...")
+        while True: 
             new_list = ioc_fetcher.ioc_fetcher.combine_ioc_sets()
             self.malicious_ips["ips"]=new_list
-            print(f"[+] IOC feeds refreshed. Total IPs: {len(new_list)}")
             time.sleep(interval_hours*3600)
 
     def auto_updater(self, interval_hours=2):
@@ -24,12 +22,8 @@ class ioc_manager:
 if __name__ == "__main__":
     manager = ioc_manager()
     print("In main")
-    #print(manager.malicious_ips["ips"])
-    print("Initial load done. Starting background updater...")
     manager.auto_updater(interval_hours=(10 / 3600))  # 10 seconds
+    print ("Done updating")
 
-    # Keep main thread alive so you can watch updates
-    while True:
-        time.sleep(5)
-        print(f"Currently {len(manager.malicious_ips['ips'])} IPs in memory.")
+
 
