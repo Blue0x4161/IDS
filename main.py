97d3df7c6b7d63b4c018a0fde7832ecf33f2ed2a
offline707 <ayataabueid@gmail.com>
2025-11-22 21:14:54 +0300

    Uncaged the watchdog

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import packet_processor
 import ioc_fetcher
 import ioc_manager
 import packet_capture
-from scapy.all import IP  #for testing
-import time #for testing
+from scapy.all import IP  
+import time 
 
 manager= ioc_manager.ioc_manager()
 print(f"Initial load done: {len(manager.malicious_ips['ips'])} IPs loaded")
-print("Starting background updater ...")
-manager.auto_updater(interval_hours=(10/3600))   #10 seconds for testing
 
-#test packet
-#packet= IP(src="80.94.93.119", dst="8.8.8.8")
+manager.auto_updater(interval_hours=(2))   
 pcap = packet_capture.packet_capture()
 pcap.start_capture("wlp4s0")
 
-print("sniffing for 10 seconds ....")
-time.sleep(10)
+#print("sniffing for 10 seconds ....")
 
-while not pcap.packet_queue.empty():
-    packet = pcap.packet_queue.get()
-    packet_processor.process_packet(packet,manager.malicious_ips)
-
-
+while True:
+    try:
+        if not pcap.packet_queue.empty():
+            packet = pcap.packet_queue.get()
+            packet_processor.process_packet(packet, manager.malicious_ips)
+        else:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("\n[!] Stopping IDS...")
+        pcap.stop()
+        break
