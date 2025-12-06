88dd76fad369020328bfcecfd3d065d2bb18d4b3
offline707 <ayataabueid@gmail.com>
2025-12-07 00:31:39 +0300

    I didnt change anything but Git keeps insisting I did

--- ioc_fetcher.py
+++ ioc_fetcher.py
@@ -1,25 +1,26 @@
+
 import requests
 
-#This is used to fetch the bad ips from these two lists i found on github
+
+# This is used to fetch the bad ips from these two lists i found on github
 class ioc_fetcher:
 
     @staticmethod
     def fetch_ipsum():
         url = "https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt"
         try:
-            response = requests.get(url , timeout=10)
-            response.raise_for_status()  #throws an exception for 5xx and 4xx errors
-            ips=set()  # a set to save unique ip inside it.
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()  # throws an exception for 5xx and 4xx errors
+            ips = set()  # a set to save unique ip inside it.
             for line in response.text.splitlines():
                 if line and not line.startswith("#"):
                     parts = line.split()
                     ips.add(parts[0].strip())
             return ips
-            
+
         except requests.RequestException as e:
             print(f"[!] Failed to fetch IPsum list: {e}")
             return set()
-
 
     @staticmethod
     def fetch_bitwire():
@@ -44,5 +45,3 @@ class ioc_fetcher:
         bitwire_ips = ioc_fetcher.fetch_bitwire()
         combined = ipsum_ips.union(bitwire_ips)
         return combined
-
-
