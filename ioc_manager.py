c814cdcf8985ceb1f1a777dfd0586743b0a5d9d0
offline707 <ayataabueid@gmail.com>
2025-12-07 00:16:50 +0300

    refactored the code

--- ioc_manager.py
+++ ioc_manager.py
@@ -1,5 +1,7 @@
+
 import threading
 import time
+
 import ioc_fetcher
 
 
@@ -9,21 +11,18 @@ class ioc_manager:
         self.malicious_ips = {"ips": ioc_fetcher.ioc_fetcher.combine_ioc_sets()}
 
     def auto_update(self, interval_hours=2):
-        while True: 
+        while True:
             new_list = ioc_fetcher.ioc_fetcher.combine_ioc_sets()
-            self.malicious_ips["ips"]=new_list
-            time.sleep(interval_hours*3600)
+            self.malicious_ips["ips"] = new_list
+            time.sleep(interval_hours * 3600)
 
     def auto_updater(self, interval_hours=2):
-        thread = threading.Thread(target = self.auto_update, args = (interval_hours,), daemon = True)
+        thread = threading.Thread(target=self.auto_update, args=(interval_hours,), daemon=True)
         thread.start()
-    
+
 
 if __name__ == "__main__":
     manager = ioc_manager()
     print("In main")
     manager.auto_updater(interval_hours=(10 / 3600))  # 10 seconds
-    print ("Done updating")
-
-
-
+    print("Done updating")
